4dda3760554949c8c4886df35aeb13ca426b62ec
Aubis Sanchez <aubisst@gmail.com>
2023-09-05 18:36:11 -0400

    feat: dynamic content  for any meal category route

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -3,19 +3,21 @@ import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
+import '../models/category.dart';
+
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(
-    BuildContext context,
-    String title,
-  ) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (ctx) => const MealsScreen(
-          meals: dummyMeals,
-          title: 'Some title...',
+        builder: (ctx) => MealsScreen(
+          meals: filteredMeals,
+          title: category.title,
         ),
       ),
     );
@@ -40,7 +42,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectCategory: () {
-                _selectCategory(context, category.title);
+                _selectCategory(context, category);
               },
             )
         ],
